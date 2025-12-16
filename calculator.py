53890b31511513fdbbd966ee619e084964a88c0f
urunbaevhusan83-tech <urunbaevhusan83@gmail.com>
2025-12-16 12:44:28 +0800

    Create calculator.py
    "Initial commit: add README and basic add function".

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a = int(input('Введите первое число: '))
+b = int(input('Введите второе число: '))
+
+def add(a,b):
+
+    x = a + b
+    print(x)
+
+def subtract(a, b):
+    x = a - b
+    print(x)
+
+subtract(a, b)
